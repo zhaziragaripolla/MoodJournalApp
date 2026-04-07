35a9b1ce81531f39c1a34ed822c76166bd7347b7
punti <g.zhazira@gmail.com>
2026-04-07 13:24:16 -0400

    Initial Commit

---
+++ MoodJournal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoodJournal
+//
+//  Created by Punti on 4/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoodJournal/MoodJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodJournalApp.swift
+//  MoodJournal
+//
+//  Created by Punti on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MoodJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
